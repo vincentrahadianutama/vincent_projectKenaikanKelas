4d8f3b632ba4bee4098e2d8943a1cade324affe8
vincentrahadianutama <vincentutama38@smk.belajar.id>
2023-06-18 10:51:51 +0700

    mencoba todo

--- database/seeders/SampleAccountsSeeder.php
+++ database/seeders/SampleAccountsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+
 use run;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -15,8 +16,8 @@ class SampleAccountsSeeder extends Seeder
     public function run(): void
     {
         User::insert([
-            'name' => 'John Doe',
-            'email' => 'johndoe@example.com',
+            'name' => 'Johan Liebert',
+            'email' => 'jliebert@gmail.com',
             'password' => bcrypt('password'),
         ]);
         
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,12 @@ use App\Http\Controllers\Api\CategoriesController;
 use Database\Seeders\SampleAccountsSeeder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
+use PHPOpenSourceSaver\JWTAuth\JWTGuard;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Auth\GenericUser;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +31,21 @@ Route::post("/categories/create", [CategoriesController::class, 'createCategorie
 Route::put("/categories/{categoriesId}/update", [CategoriesController::class, 'updateCategories']);
 Route::delete("/categories/{categoriesId}/delete", [CategoriesController::class, "deleteCategories"]);
 Route::post("/categories/account", [SampleAccountsSeeder::class, "run"]);
+Route::post("/categories/account", [SampleAccountsSeeder::class, "run"]);
+Route::post("/categories/account", [SampleAccountsSeeder::class, "run"]);
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+});
+
+Route::controller(TodoController::class)->group(function () {
+    Route::get('todos', 'index');
+    Route::post('todo', 'store');
+    Route::get('todo/{id}', 'show');
+    Route::put('todo/{id}', 'update');
+    Route::delete('todo/{id}', 'destroy');
+}); 
